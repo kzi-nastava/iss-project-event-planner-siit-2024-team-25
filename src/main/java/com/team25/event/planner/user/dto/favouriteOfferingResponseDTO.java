81fe702ec83a2e812e8621395a7cf7c6cea48020
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2024-11-23 21:37:34 +0100

    [REFACTOR] method names

--- src/main/java/com/team25/event/planner/user/dto/favouriteOfferingResponseDTO.java
+++ src/main/java/com/team25/event/planner/user/dto/favouriteOfferingResponseDTO.java
@@ -3,7 +3,7 @@ package com.team25.event.planner.user.dto;
 import lombok.Data;
 
 @Data
-public class FavoriteOfferingResponseDTO {
+public class favouriteOfferingResponseDTO {
     private Long id;
     private String name;
     private String description;
