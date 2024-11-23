9ee1d1773c24f08240aba647b0649bfb1d82f3a7
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-11-23 07:27:24 +0100

    [ADD] Custom exceptions and global exception handler

---
+++ src/main/java/com/team25/event/planner/common/dto/ErrorResponseDTO.java
@@ -0,0 +1,30 @@
+package com.team25.event.planner.common.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@RequiredArgsConstructor
+public class ErrorResponseDTO {
+    private final Integer code;
+    private final String message;
+    private final Map<String, String> errors;
+
+    public ErrorResponseDTO(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+        this.errors = new HashMap<>();
+    }
+
+    public Map<String, String> getErrors() {
+        return Collections.unmodifiableMap(errors);
+    }
+
+    public void addError(String key, String message) {
+        errors.put(key, message);
+    }
+}
