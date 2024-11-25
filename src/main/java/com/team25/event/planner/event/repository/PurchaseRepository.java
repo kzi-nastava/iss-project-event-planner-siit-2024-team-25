750e5100f956cb44ebf163f9ce13890294e70a7b
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-25 18:51:15 +0100

    [REFACTOR] Purchase model

--- src/main/java/com/team25/event/planner/event/repository/PurchaseRepository.java
+++ src/main/java/com/team25/event/planner/event/repository/PurchaseRepository.java
@@ -1,8 +1,7 @@
 package com.team25.event.planner.event.repository;
 
-import com.team25.event.planner.offering.common.model.Purchase;
+import com.team25.event.planner.event.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 }
