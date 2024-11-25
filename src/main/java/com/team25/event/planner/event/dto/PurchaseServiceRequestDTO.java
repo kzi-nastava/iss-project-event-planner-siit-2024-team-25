da20bbc523aad396c7dea024cb0cab071655466e
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-25 18:54:16 +0100

    [ADD] DTO classes

---
+++ src/main/java/com/team25/event/planner/event/dto/PurchaseServiceRequestDTO.java
@@ -0,0 +1,18 @@
+package com.team25.event.planner.event.dto;
+
+import com.team25.event.planner.offering.common.model.OfferingCategory;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class PurchaseServiceRequestDTO {
+
+    private LocalDate startDate;
+    private LocalTime startTime;
+    private LocalDate endDate;
+    private LocalTime endTime;
+    private double price;
+    private Long offeringCategoryId;
+}
---
+++ src/main/java/com/team25/event/planner/event/dto/PurchaseServiceResponseDTO.java
@@ -0,0 +1,19 @@
+package com.team25.event.planner.event.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class PurchaseServiceResponseDTO {
+    private Long id;
+    private LocalDate startDate;
+    private LocalTime startTime;
+    private LocalDate endDate;
+    private LocalTime endTime;
+    private double price;
+    private Long offeringCategoryId;
+    private Long eventId;
+    private Long productId;
+}
