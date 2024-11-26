ce913c8d03a62406849a8d56e764611e12be9ca8
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-26 12:29:56 +0100

    [REFACTOR] Controller name

--- src/main/java/com/team25/event/planner/user/controller/ReportController.java
+++ src/main/java/com/team25/event/planner/user/controller/ReportController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/users")
 @AllArgsConstructor
-public class UserReportController {
+public class ReportController {
     private final ReportService reportService;
 
     @PostMapping(value = "/{reportedUserId}/report")
