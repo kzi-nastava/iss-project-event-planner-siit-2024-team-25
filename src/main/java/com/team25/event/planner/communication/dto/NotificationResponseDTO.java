d9de48b8d0bf906830d6b8c6fb4ceda8e03547a6
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-27 13:04:31 +0100

    [ADD] DTOs

--- src/main/java/com/team25/event/planner/communication/dto/NotificationResponseDTO.java
+++ src/main/java/com/team25/event/planner/communication/dto/NotificationResponseDTO.java
@@ -1,4 +1,18 @@
 package com.team25.event.planner.communication.dto;
 
+import com.team25.event.planner.communication.model.NotificationCategory;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class NotificationResponseDTO {
+    private Long id;
+    private Long entityId;
+    private NotificationCategory notificationCategory;
+    private String message;
+    private LocalDateTime createdDate;
+    private Boolean isViewed;
+
+
 }
