0c94449d4b4de6eb38efc90ef9769a73b3c81e74
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-12-02 18:28:21 +0100

    [CHANGE] DB migration and core models

--- src/main/java/com/team25/event/planner/offering/service/model/Service.java
+++ src/main/java/com/team25/event/planner/offering/service/model/Service.java
@@ -1,10 +1,15 @@
 package com.team25.event.planner.offering.service.model;
 
+import com.team25.event.planner.event.model.EventType;
 import com.team25.event.planner.offering.common.model.Offering;
+import com.team25.event.planner.offering.common.model.OfferingCategory;
+import com.team25.event.planner.offering.common.model.OfferingType;
+import com.team25.event.planner.user.model.Owner;
 import com.team25.event.planner.user.model.User;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -15,6 +20,18 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 public class Service extends Offering{
+    public Service(Long id, String name, String description, double price, double discount, List<String> images, boolean isVisible,
+                   boolean isAvailable, OfferingType status, Collection<EventType> eventTypes, OfferingCategory offeringCategory, Owner owner,
+                   String specifics, int duration, int reservationDeadline, int cancellationDeadline, ReservationType reservationType, List<User> users) {
+        super(id, name, description, price, discount, images, isVisible, isAvailable, status, eventTypes, offeringCategory, owner);
+        this.specifics = specifics;
+        this.duration = duration;
+        this.reservationDeadline = reservationDeadline;
+        this.cancellationDeadline = cancellationDeadline;
+        this.reservationType = reservationType;
+        this.users = users;
+    }
+
     private String specifics;
     private int duration;
     private int reservationDeadline;
