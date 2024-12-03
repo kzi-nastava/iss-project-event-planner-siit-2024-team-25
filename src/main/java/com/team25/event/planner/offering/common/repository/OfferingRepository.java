8c2ad50ea399e57c15ab697114c067c4ac9e728e
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-12-03 13:23:35 +0100

    [ADD] Repository function

--- src/main/java/com/team25/event/planner/offering/common/repository/OfferingRepository.java
+++ src/main/java/com/team25/event/planner/offering/common/repository/OfferingRepository.java
@@ -2,6 +2,8 @@ package com.team25.event.planner.offering.common.repository;
 
 import com.team25.event.planner.offering.common.dto.OfferingPreviewResponseDTO;
 import com.team25.event.planner.offering.common.model.Offering;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -26,4 +28,24 @@ public interface OfferingRepository extends JpaRepository<Offering, Long>, JpaSp
             "WHERE o IN :offerings " +
             "GROUP BY o.id, o.name, o.owner.firstName, o.owner.lastName, o.description, o.owner.companyAddress.country, o.owner.companyAddress.city, o.price")
     List<OfferingPreviewResponseDTO> findOfferingsWithAverageRating(@Param("offerings") List<Offering> offerings);
+
+    @Query("SELECT new com.team25.event.planner.offering.common.dto.OfferingPreviewResponseDTO(" +
+            "o.id, " +
+            "o.name, " +
+            "o.owner.firstName || ' ' || o.owner.lastName, " +
+            "o.description, " +
+            "o.owner.companyAddress.country, " +
+            "o.owner.companyAddress.city, " +
+            "COALESCE(AVG(r.rating), 0), " +
+            "o.price) " +
+            "FROM Offering o " +
+            "LEFT JOIN Purchase p ON p.offering.id = o.id " +
+            "LEFT JOIN OfferingReview r ON r.purchase.id = p.id " +
+            "WHERE " +
+            "(:country IS NULL OR :country = '' OR o.owner.companyAddress.country = :country) AND " +
+            "(:city IS NULL OR :city = '' OR o.owner.companyAddress.city = :city) " +
+            "AND (r.status = com.team25.event.planner.common.model.ReviewStatus.APPROVED)"+
+            "GROUP BY o.id, o.name, o.owner.firstName, o.owner.lastName, o.description, o.owner.companyAddress.country, o.owner.companyAddress.city, o.price " +
+            "ORDER BY COALESCE(AVG(r.rating), 0) DESC")
+    Page<OfferingPreviewResponseDTO> findTopOfferings(String country, String city, Pageable pageable);
 }
