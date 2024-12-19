1fee31eb5091a8e18c1ce28bdcce9d07d83e082d
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2024-12-19 19:49:10 +0100

    [ADD] controller and service

---
+++ src/main/java/com/team25/event/planner/event/controller/BudgetItemController.java
@@ -0,0 +1,49 @@
+package com.team25.event.planner.event.controller;
+
+import com.team25.event.planner.event.dto.BudgetItemRequestDTO;
+import com.team25.event.planner.event.dto.BudgetItemResponseDTO;
+import com.team25.event.planner.event.service.BudgetItemService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/budget-items/")
+public class BudgetItemController {
+    private final BudgetItemService budgetItemService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("EVENT_ORGANIZER")
+    public List<BudgetItemResponseDTO> getBudgetItems() {
+        return budgetItemService.getAllBudgetItems();
+    }
+
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("EVENT_ORGANIZER")
+    public BudgetItemResponseDTO getBudgetItemById(@PathVariable Long id) {
+        return budgetItemService.getBudgetItemById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("EVENT_ORGANIZER")
+    public BudgetItemResponseDTO createBudgetItem(@RequestBody BudgetItemRequestDTO budgetItemRequestDTO) {
+        return budgetItemService.createBudgetItem(budgetItemRequestDTO);
+    }
+
+    @PutMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("EVENT_ORGANIZER")
+    public BudgetItemResponseDTO updateBudgetItem(@PathVariable Long id, @RequestBody BudgetItemRequestDTO budgetItemRequestDTO) {
+        return budgetItemService.updateBudgetItem(id, budgetItemRequestDTO);
+    }
+
+    @DeleteMapping(value = "{id}")
+    @Secured("EVENT_ORGANIZER")
+    public ResponseEntity<?> deleteBudgetItem(@PathVariable Long id) {
+        return budgetItemService.deleteBudgetItem(id);
+    }
+}
--- src/main/java/com/team25/event/planner/event/service/BudgetItemService.java
+++ src/main/java/com/team25/event/planner/event/service/BudgetItemService.java
@@ -1,16 +1,46 @@
 package com.team25.event.planner.event.service;
 
+import com.team25.event.planner.common.exception.NotFoundError;
+import com.team25.event.planner.event.dto.BudgetItemRequestDTO;
 import com.team25.event.planner.event.dto.BudgetItemResponseDTO;
+import com.team25.event.planner.event.mapper.BudgetItemMapper;
+import com.team25.event.planner.event.model.BudgetItem;
+import com.team25.event.planner.event.model.Money;
 import com.team25.event.planner.event.repository.BudgetItemRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class BudgetItemService {
     private final BudgetItemRepository budgetItemRepository;
+    private final BudgetItemMapper budgetItemMapper;
 
+    public List<BudgetItemResponseDTO> getAllBudgetItems() {
+        return budgetItemRepository.findAll().stream().map(budgetItemMapper::toResponseDTO).collect(Collectors.toList());
+    }
 
+    public BudgetItemResponseDTO getBudgetItemById(Long id) {
+        BudgetItem budgetItem = budgetItemRepository.findById(id).orElseThrow(()-> new NotFoundError("Budget item not found"));
+        return budgetItemMapper.toResponseDTO(budgetItem);
+    }
+
+    public BudgetItemResponseDTO createBudgetItem(BudgetItemRequestDTO budgetItemRequestDTO) {
+        BudgetItem budgetItem = budgetItemMapper.toBudgetItem(budgetItemRequestDTO);
+        return budgetItemMapper.toResponseDTO(budgetItemRepository.save(budgetItem));
+    }
+
+    public BudgetItemResponseDTO updateBudgetItem(Long id, BudgetItemRequestDTO budgetItemRequestDTO) {
+        BudgetItem budgetItem = budgetItemRepository.findById(id).orElseThrow(()-> new NotFoundError("Budget item not found"));
+        budgetItem.setMoney(new Money(budgetItemRequestDTO.getBudget(), "RSD"));
+        return budgetItemMapper.toResponseDTO(budgetItemRepository.save(budgetItem));
+    }
+    public ResponseEntity<?> deleteBudgetItem(Long id) {
+        budgetItemRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
