cbff95140cf0d4fcd98ea9447de43ad6919e5323
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2025-01-13 00:53:41 +0100

    [ADD] get chats by user

--- src/main/java/com/team25/event/planner/communication/repository/ChatRoomRepository.java
+++ src/main/java/com/team25/event/planner/communication/repository/ChatRoomRepository.java
@@ -1,10 +1,14 @@
 package com.team25.event.planner.communication.repository;
 
 import com.team25.event.planner.communication.model.ChatRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
+
+    Page<ChatRoom> findAllBySenderId(Long senderId, Pageable pageable);
 }
