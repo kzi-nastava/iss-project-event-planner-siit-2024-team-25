81e498624106cab2e84d95e2baac0e4260511322
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-01-18 19:09:40 +0100

    [ADD] Send new review notification

--- src/main/java/com/team25/event/planner/common/service/ReviewService.java
+++ src/main/java/com/team25/event/planner/common/service/ReviewService.java
@@ -8,8 +8,11 @@ import com.team25.event.planner.common.exception.NotFoundError;
 import com.team25.event.planner.common.mapper.ReviewMapper;
 import com.team25.event.planner.common.model.Review;
 import com.team25.event.planner.common.model.ReviewStatus;
+import com.team25.event.planner.common.model.ReviewType;
 import com.team25.event.planner.common.repository.ReviewRepository;
 import com.team25.event.planner.common.specification.ReviewSpecification;
+import com.team25.event.planner.communication.model.NotificationCategory;
+import com.team25.event.planner.communication.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
@@ -22,6 +25,7 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ReviewSpecification reviewSpecification;
     private final ReviewMapper reviewMapper;
+    private final NotificationService notificationService;
 
     public ReviewResponseDTO createReview(ReviewRequestDTO reviewRequestDTO) {
         Review review = reviewMapper.reviewFromDTO(reviewRequestDTO);
@@ -32,6 +36,13 @@ public class ReviewService {
     public ReviewResponseDTO updateReview(ReviewUpdateRequestDTO reviewRequestDTO, Long id) {
         Review review = reviewRepository.findById(id).orElseThrow(()-> new NotFoundError("Not found review"));
         review.setReviewStatus(reviewRequestDTO.getReviewStatus());
+        if(review.getReviewStatus().equals(ReviewStatus.APPROVED)){
+            if(review.getReviewType().equals(ReviewType.EVENT_REVIEW)){
+                notificationService.sendEventReviewNotificationToEventOrganizer(review.getPurchase().getEvent());
+            }else{
+                notificationService.sendOfferingReviewNotificationToOwner(review.getPurchase().getOffering());
+            }
+        }
         return reviewMapper.toDTO(reviewRepository.save(review));
     }
     public Page<ReviewResponseDTO> getReviews(ReviewFilterDTO filter,int page, int size, String sortBy, String sortDirection) {
