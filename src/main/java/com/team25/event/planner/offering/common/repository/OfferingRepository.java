fff6db9737e666e675dd0f41a6ab96ff3065b1cd
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-06-17 17:51:31 +0200

    [BUGFIX] Find top offerings

--- src/main/java/com/team25/event/planner/offering/common/repository/OfferingRepository.java
+++ src/main/java/com/team25/event/planner/offering/common/repository/OfferingRepository.java
@@ -65,45 +65,46 @@ public interface OfferingRepository extends JpaRepository<Offering, Long>, JpaSp
 
 
     @Query("""
-    SELECT new com.team25.event.planner.offering.common.dto.OfferingPreviewResponseDTO(
-        o.id,
-        o.name,
-        o.owner.firstName || ' ' || o.owner.lastName,
-        o.description,
-        o.owner.companyAddress.country,
-        o.owner.companyAddress.city,
-        COALESCE(AVG(r.rating), 0),
-        o.price,
-        (CASE WHEN (s.id IS NULL) THEN false ELSE true END),
-        (CASE 
-            WHEN EXISTS (
-                SELECT 1 FROM User u1 
-                JOIN u1.favoriteServices fs 
-                WHERE u1.id = :currentUserId AND fs.id = o.id
-            )
-            OR EXISTS (
-                SELECT 1 FROM User u2 
-                JOIN u2.favoriteProducts fp 
-                WHERE u2.id = :currentUserId AND fp.id = o.id
-            )
-            THEN true ELSE false END)
-    )
-    FROM Offering o
-    LEFT JOIN Service s ON s.id = o.id
-    LEFT JOIN Purchase p ON p.offering.id = o.id
-    LEFT JOIN Review r ON r.purchase.id = p.id
-    WHERE r.reviewType = com.team25.event.planner.common.model.ReviewType.OFFERING_REVIEW AND
-        (:country IS NULL OR :country = '' OR o.owner.companyAddress.country = :country) AND
-        (:city IS NULL OR :city = '' OR o.owner.companyAddress.city = :city) AND
-        (:currentUserId IS NULL OR
-            NOT EXISTS (
-                SELECT 1 FROM User u 
-                WHERE u.id = :currentUserId AND u MEMBER OF o.owner.blockedUsers
-            )
+SELECT new com.team25.event.planner.offering.common.dto.OfferingPreviewResponseDTO(
+    o.id,
+    o.name,
+    o.owner.firstName || ' ' || o.owner.lastName,
+    o.description,
+    o.owner.companyAddress.country,
+    o.owner.companyAddress.city,
+    COALESCE(AVG(r.rating), 0),
+    o.price,
+    (CASE WHEN (s.id IS NULL) THEN false ELSE true END),
+    (CASE 
+        WHEN EXISTS (
+            SELECT 1 FROM User u1 
+            JOIN u1.favoriteServices fs 
+            WHERE u1.id = :currentUserId AND fs.id = o.id
         )
-    GROUP BY o.id, o.name, o.owner.firstName, o.owner.lastName, o.description, 
-             o.owner.companyAddress.country, o.owner.companyAddress.city, o.price, s.id
-    ORDER BY COALESCE(AVG(r.rating), 0) DESC
+        OR EXISTS (
+            SELECT 1 FROM User u2 
+            JOIN u2.favoriteProducts fp 
+            WHERE u2.id = :currentUserId AND fp.id = o.id
+        )
+        THEN true ELSE false END)
+)
+FROM Offering o
+LEFT JOIN Service s ON s.id = o.id
+LEFT JOIN Purchase p ON p.offering.id = o.id
+LEFT JOIN Review r ON r.purchase.id = p.id 
+    AND r.reviewType = com.team25.event.planner.common.model.ReviewType.OFFERING_REVIEW
+WHERE
+    (:country IS NULL OR :country = '' OR o.owner.companyAddress.country = :country) AND
+    (:city IS NULL OR :city = '' OR o.owner.companyAddress.city = :city) AND
+    (:currentUserId IS NULL OR
+        NOT EXISTS (
+            SELECT 1 FROM User u 
+            WHERE u.id = :currentUserId AND u MEMBER OF o.owner.blockedUsers
+        )
+    )
+GROUP BY o.id, o.name, o.owner.firstName, o.owner.lastName, o.description, 
+         o.owner.companyAddress.country, o.owner.companyAddress.city, o.price, s.id
+ORDER BY COALESCE(AVG(r.rating), 0) DESC
 """)
     Page<OfferingPreviewResponseDTO> findTopOfferings(String country, String city, Long currentUserId, Pageable pageable);
 
